dc74b0f8bc7b4fd3c58b574451fe101297530459
JaimitoSky <edgar070704@hotmail.com>
2025-09-18 05:54:21 -0500

    Avance 1, creación de esquema y repositorio

---
+++ app/src/main/java/com/example/lab2_20211602_iot/data/model/Device.java
@@ -0,0 +1,4 @@
+package com.example.lab2_20211602_iot.data.model;
+
+public class Device {
+}
---
+++ app/src/main/java/com/example/lab2_20211602_iot/ui/routers/RouterListActivity.java
@@ -0,0 +1,4 @@
+package com.example.lab2_20211602_iot.ui.routers;
+
+public class RouterListActivity {
+}
